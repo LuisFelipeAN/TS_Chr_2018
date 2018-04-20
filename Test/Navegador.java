f841fcb293e1c74e2fbeb4374f6ef1748dabac66
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-04-20 20:31:28 -0300

    commite inicial

---
+++ Test/Navegador.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navegador {
+	
+	static WebDriver driver;
+	
+	@BeforeClass
+	public static void configura(){
+		driver = (WebDriver) new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver","/ice/Downloads/Librarises/chromedriver");
+		driver.get("https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Fwww.google.com.br%2Fsearch%3Fei%3DSnPaWt_KMsKUwATx2L7oBA%26q%3DMHmarvin%26oq%3DMHmarvin%26gs_l%3Dpsy-ab.3..0i13k1l10.17215.27605.0.27941.8.8.0.0.0.0.138.946.2j6.8.0....0...1.1.64.psy-ab..0.8.943...0j0i13i10k1j0i10k1j0i30k1.0.aa8n07YP6bI&hl=pt-BR&flowName=GlifWebSignIn&flowEntry=SignUp");
+		
+	}
+	@Test
+	public static void testaPagina(){
+		WebElement nome = driver.findElement(By.id("firstName"));
+		nome.sendKeys("Luis Felipe");
+	}
+}
