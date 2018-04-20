183547ada81d1443062932d522d57e545e4e72a0
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-04-20 20:51:08 -0300

    update program

--- Test/Navegador.java
+++ Test/Navegador.java
@@ -1,7 +1,13 @@
-import org.junit.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import org.openqa.selenium.*;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
 
 public class Navegador {
 	
